3e6529d379f0c23344f2af893a55b298d2e95528
Evgeniy Khyst <evgeniy.khist@gmail.com>
2018-05-07 20:52:07 +0300

    Parametrize Kafka embedded port, update description

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
+This sample demonstrates how to use Kafka transactions in Spring Boot applications and synchronize them with database transactions.
+
 Run this sample using
 
 ```
-./gradlew clean test -i
+./gradlew clean test -i -Dkafka.embedded.port=9092
 ```
--- src/test/java/com/example/kafka/sample/transaction/config/KafkaEmbeddedConfig.java
+++ src/test/java/com/example/kafka/sample/transaction/config/KafkaEmbeddedConfig.java
@@ -4,6 +4,7 @@ import static com.example.kafka.sample.transaction.listener.TestKafkaListener.IN
 import static com.example.kafka.sample.transaction.listener.TestKafkaListener.OUTPUT_TEST_TOPIC;
 
 import kafka.server.KafkaConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
@@ -11,11 +12,14 @@ import org.springframework.kafka.test.rule.KafkaEmbedded;
 @Configuration
 public class KafkaEmbeddedConfig {
 
+  @Value("${kafka.embedded.port:9092}")
+  private int kafkaEmbeddedPort;
+
   @Bean
   public KafkaEmbedded kafkaEmbedded() {
     KafkaEmbedded kafkaEmbedded = new KafkaEmbedded(1, false, 1,
         INPUT_TEST_TOPIC, OUTPUT_TEST_TOPIC);
-    kafkaEmbedded.setKafkaPorts(9092);
+    kafkaEmbedded.setKafkaPorts(kafkaEmbeddedPort);
     kafkaEmbedded.brokerProperty(KafkaConfig.TransactionsTopicReplicationFactorProp(), "1");
     kafkaEmbedded.brokerProperty(KafkaConfig.TransactionsTopicMinISRProp(), "1");
     return kafkaEmbedded;
